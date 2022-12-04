66ca6c60e9184babe72ba7434d527baf870be7fe
eternaleight <eternaleight0@gmail.com>
2022-12-05 00:42:47 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,15 @@ import Image from "next/image"
 
 const Home: NextPage = () => {
   return (
-    <div className="relative flex flex-col mx-auto max-w-[1680px] items-center justify-center min-h-screen">
-      <div className="bg-white w-full h-[60px] flex items-center justify-end pr-3 gap-3">
-        <div className="absolute pl-3 top-[14px] left-[0px]">
-          <Image src="/sampleLogo.png" height={32} width={122} />
-        </div>
+    <div className="relative flex flex-col mx-auto max-w-[1680px] items-center min-h-screen bg-[#eee]">
+      <div className="bg-white w-full h-[60px] flex items-center justify-end pr-3 gap-3 absolute">
         <button className="w-[130px] h-[40px] bg-white hover:bg-[#18a0fb] border-[1px] border-[#18a0fb] text-[#18a0fb] hover:text-[#18a0fbee] hover:bg-[#18a0fb2e]">
           Log in
         </button>
         <button className="w-[130px] h-[40px]">Register</button>
+        <div className="absolute pl-3 top-[14px] left-[0px]">
+          <Image src="/sampleLogo.png" height={32} width={122} />
+        </div>
       </div>
 
       <Head>
@@ -20,74 +20,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+      <footer>
       </footer>
     </div>
   )
