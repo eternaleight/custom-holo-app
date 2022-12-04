3a9cb3d5157f42f66ce5b3178e55e76c87321ddb
eternaleight <eternaleight0@gmail.com>
2022-12-05 00:52:46 +0900

    fix

---
+++ components/header.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import Head from "next/head"
+
+const Header = () => {
+  return (
+    <>
+      <div className="bg-white w-full h-[60px] flex items-center justify-end pr-3 gap-3 absolute">
+        <button className="w-[130px] h-[40px] bg-white hover:bg-[#18a0fb] border-[1px] border-[#18a0fb] text-[#18a0fb] hover:text-[#18a0fbee] hover:bg-[#18a0fb2e]">
+          Log in
+        </button>
+        <button className="w-[130px] h-[40px]">Register</button>
+        <div className="absolute pl-3 top-[14px] left-[0px]">
+          <Image src="/sampleLogo.png" height={32} width={122} />
+        </div>
+      </div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,12 @@
 import type { NextPage } from "next"
-import Head from "next/head"
-import Image from "next/image"
+import Header from "../components/header"
 
 const Home: NextPage = () => {
   return (
     <div className="relative flex flex-col mx-auto max-w-[1680px] items-center min-h-screen bg-[#eee]">
-      <div className="bg-white w-full h-[60px] flex items-center justify-end pr-3 gap-3 absolute">
-        <button className="w-[130px] h-[40px] bg-white hover:bg-[#18a0fb] border-[1px] border-[#18a0fb] text-[#18a0fb] hover:text-[#18a0fbee] hover:bg-[#18a0fb2e]">
-          Log in
-        </button>
-        <button className="w-[130px] h-[40px]">Register</button>
-        <div className="absolute pl-3 top-[14px] left-[0px]">
-          <Image src="/sampleLogo.png" height={32} width={122} />
-        </div>
-      </div>
-
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+      <Header/>
       <main>
       </main>
-
       <footer>
       </footer>
     </div>
