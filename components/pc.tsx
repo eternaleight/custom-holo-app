e2553bfc4819fe21494a1780d78686777f9b98ef
eternaleight <eternaleight0@gmail.com>
2022-12-31 22:31:02 +0900

    fix

--- components/pc.tsx
+++ components/pc.tsx
@@ -4,7 +4,7 @@ interface Text {
 const Pc = ({ addText }: Text) => {
   return (
     <>
-      <figure className="mx-auto my-20 relative z-[1] max-w-full w-[50rem] h-[480px] rounded-b-lg shadow-[0_2.75rem_3.5rem_-2rem_rgb(45_55_75_/_20%),_0_0_5rem_-2rem_rgb(45_55_75_/_15%)] dark:shadow-[0_2.75rem_3.5rem_-2rem_rgb(0_0_0_/_20%),_0_0_5rem_-2rem_rgb(0_0_0_/_15%)]">
+      <figure className="mx-auto my-20 relative z-[1] max-w-full w-[50rem] h-[480px] rounded-b-lg ">
         <div className="relative flex items-center max-w-[50rem] bg-gray-800 rounded-t-lg py-2 px-24 dark:bg-gray-700">
           <div className="absolute flex space-x-1 top-2/4 left-4 -translate-y-1">
             <span className="w-2 h-2 bg-gray-600 rounded-full dark:bg-gray-600"></span>
@@ -15,7 +15,7 @@ const Pc = ({ addText }: Text) => {
         </div>
 
         <div className="bg-gray-800 rounded-b-lg">
-          <img className="h-auto max-w-full rounded-b-lg" src="/rectangle.png" alt="Image Description" />
+          <img className="h-auto max-w-full rounded-b-lg shadow-[0_2.75rem_3.5rem_-2rem_rgb(45_55_75_/_20%),_0_0_5rem_-2rem_rgb(45_55_75_/_15%)] dark:shadow-[0_2.75rem_3.5rem_-2rem_rgb(0_0_0_/_20%),_0_0_5rem_-2rem_rgb(0_0_0_/_15%)]" src="/rectangle.png" alt="Image Description" />
         </div>
       </figure>
     </>
