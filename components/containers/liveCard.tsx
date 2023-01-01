b5dc06d426b655052198220be1e61b22cd769786
eternaleight <eternaleight0@gmail.com>
2023-01-01 21:29:08 +0900

    fix

--- components/containers/liveCard.tsx
+++ components/containers/liveCard.tsx
@@ -77,16 +77,14 @@ const LiveCard = () => {
           holoDatas.channel.id !== "" &&
           holoDatas.channel.id !== "UCWsfcksUUpoEvhia0_ut0bA" ? (
           <>
-            <div className="w-[290px] max-md:w-[290px] max-sm:w-[240px] max-xs:w-full h-full flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+            <div className="w-[290px] max-md:w-[290px] max-sm:w-[240px] max-xs:w-[48%] h-full flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+              <a href={`${holoVideo}${holoDatas.id}`}>
               <img className="w-full h-auto rounded-t-xl" src={youtube_jpeg + holoDatas.id + youtube_jpeg_size.large} alt="Image Description" />
               <div className="p-4 md:p-5">
-                <h3 className="text-lg font-bold text-gray-800 dark:text-white">{holoDatas.title}</h3>
-                <div className="text-gray-400">{dayjs(holoDatas.start_scheduled).format("YYYY-MM-DD HH:mm")}</div>
-                <p className="mt-1 text-gray-800 dark:text-gray-400">{holoDatas.channel.name}</p>
-                <a className="inline-flex items-center justify-center px-4 py-3 mt-3 text-sm font-semibold text-white bg-blue-500 border border-transparent gap-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all dark:focus:ring-offset-gray-800" href={`${holoVideo}${holoDatas.id}`} target="_blank">
-                  Go stream
-                </a>
+                <h3 className="flex font-bold text-gray-800 text-md dark:text-white">{holoDatas.title}</h3>
+                <div className="text-gray-400">{dayjs(holoDatas.start_scheduled).format("HH:mm")}</div>
               </div>
+              </a>
             </div>
           </>
         ) : null
