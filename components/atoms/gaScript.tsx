8fdea6b194a87a9f622184dd5fcc34544d690ce2
eternaleight <eternaleight0@gmail.com>
2023-01-26 23:09:14 +0900

    feat(analytics): gtagの設置 gaScriptの作成 @types/gtag.jsのinstall

---
+++ components/atoms/gaScript.tsx
@@ -0,0 +1,24 @@
+import Script from "next/script"
+import { GA_TRACKING_ID } from "../../lib/gtag"
+
+export const GaScript = () => (
+  <>
+    {GA_TRACKING_ID && (
+      <>
+        <Script
+          id="load-ga"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="load-ga-script">
+          {`
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+             gtag('config', '${GA_TRACKING_ID}');
+          `}
+        </Script>
+      </>
+    )}
+  </>
+)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import * as gtag from "../lib/gtag"
+import { GaScript } from "../components/atoms/gaScript"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      <GaScript />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
